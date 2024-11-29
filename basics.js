ff077d9b5d565c35c7bc1c6ada1364e982ae4160
KulwinderKaur666 <kkaur85815@gmail.com>
2024-11-29 01:57:41 -0500

    Initial commit

---
+++ basics.js
@@ -0,0 +1,19 @@
+function calculateArea(width,height){
+    // Funtion to validate if the value is a positive number
+    return typeof value == 'number' && value > 0;
+}
+//Loop until valid width and provided
+    while(true){
+        if(typeof width == 'number' && widthwidth > 0 && typeof height === 'number' && height > 0) {
+            //Calculate the area
+            const area = width * height;
+            console.log("Area of the rectangle: " + area );
+            return area;
+    } else {
+        width = parseFloat(prompt("Please enter a valid width (positive number)"));
+        height = parseFloat(prompt("Please enter a valid height (positive number)"));
+    }
+}
+ 
+
+
